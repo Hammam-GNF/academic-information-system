9c721ac84988946bb0baa70329287c717e819e9c
Hammam Ghina Nur Fauzi <hammamgonjil@gmail.com>
2026-07-16 23:17:05 +0700

    feat: add base repository implementation

---
+++ app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface BaseRepositoryInterface
+{
+    public function all(array $columns = ['*']);
+
+    public function paginate(
+        int $perPage = 15,
+        array $columns = ['*']
+    );
+
+    public function find(int|string $id);
+
+    public function findOrFail(int|string $id);
+
+    public function create(array $data);
+
+    public function update(int|string $id, array $data);
+
+    public function delete(int|string $id): bool;
+}
