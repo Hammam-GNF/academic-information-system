74c88614932f04096e4b985edce6832a3ebfda3e
Hammam Ghina Nur Fauzi <hammamgonjil@gmail.com>
2026-07-19 16:55:50 +0700

    feat: add responsive admin sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,14 +4,29 @@
 
 <x-layout.head />
 
-<body class="font-sans antialiased bg-gray-100">
+<body
+    x-data="{ sidebarOpen: false }"
+    @keydown.escape.window="sidebarOpen = false"
+    class="font-sans antialiased bg-gray-100"
+>
+
+<div
+    x-show="sidebarOpen"
+    x-transition.opacity
+    x-cloak
+    @click="sidebarOpen = false"
+    class="fixed inset-0 z-40 bg-black/50 lg:hidden"
+></div>
 
 <div class="flex min-h-screen">
 
     {{-- Sidebar --}}
-    <aside class="hidden lg:flex lg:w-64 lg:flex-col bg-white border-r border-gray-200">
+    <aside
+        class="fixed inset-y-0 left-0 z-50 flex w-64 flex-col bg-white border-r border-gray-200 transform transition-transform duration-300 lg:static lg:translate-x-0"
+        :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full'"
+    >
 
-        <div class="flex items-center h-16 px-6 border-b">
+        <div class="flex items-center justify-between h-16 px-6 border-b">
 
             <a
                 href="{{ route('admin.dashboard') }}"
@@ -24,6 +39,26 @@
                 </span>
 
             </a>
+
+            <button
+                @click="sidebarOpen = false"
+                class="lg:hidden text-gray-500 hover:text-gray-900"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
 
         </div>
 
@@ -100,11 +135,35 @@
             class="flex items-center justify-between h-16 px-6 bg-white border-b border-gray-200"
         >
 
-            <h1
-                class="text-lg font-semibold text-gray-900"
-            >
-                {{ $header ?? '' }}
-            </h1>
+            <div class="flex items-center gap-4">
+
+                <button
+                    @click="sidebarOpen = true"
+                    class="lg:hidden text-gray-600 hover:text-gray-900"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                </button>
+
+                <h1
+                    class="text-lg font-semibold text-gray-900"
+                >
+                    {{ $header ?? '' }}
+                </h1>
+
+            </div>
 
             <x-navigation.dropdown>
 
@@ -171,7 +230,7 @@
 
         </header>
 
-        <main class="flex-1 p-6">
+        <main class="flex-1 overflow-auto p-6">
 
             {{ $slot }}
 
