272083aedd38719b5539ed0913be5a8b2eea408e
Hammam Ghina Nur Fauzi <hammamgonjil@gmail.com>
2026-07-21 05:57:41 +0700

    feat(notification): enhance reusable toast component

--- resources/views/components/feedback/toast.blade.php
+++ resources/views/components/feedback/toast.blade.php
@@ -8,34 +8,45 @@
 @php
 
 $type = 'success';
+$title = 'Success';
 $message = session('success');
 
-if(session()->has('error')){
-    $type='error';
-    $message=session('error');
+if (session()->has('error')) {
+    $type = 'error';
+    $title = 'Error';
+    $message = session('error');
 }
 
-if(session()->has('warning')){
-    $type='warning';
-    $message=session('warning');
+if (session()->has('warning')) {
+    $type = 'warning';
+    $title = 'Warning';
+    $message = session('warning');
 }
 
-if(session()->has('info')){
-    $type='info';
-    $message=session('info');
+if (session()->has('info')) {
+    $type = 'info';
+    $title = 'Information';
+    $message = session('info');
 }
 
-$colors=[
-    'success'=>'toast-success',
-    'error'=>'toast-error',
-    'warning'=>'toast-warning',
-    'info'=>'toast-info',
+$icons = [
+    'success' => '✓',
+    'error' => '✕',
+    'warning' => '!',
+    'info' => 'i',
+];
+
+$classes = [
+    'success' => 'toast-success',
+    'error' => 'toast-error',
+    'warning' => 'toast-warning',
+    'info' => 'toast-info',
 ];
 
 @endphp
 
 <div
-    x-data="{ show:true }"
+    x-data="{ show: true }"
     x-show="show"
     x-transition:enter="transition ease-out duration-300"
     x-transition:enter-start="translate-x-full opacity-0"
@@ -43,22 +54,34 @@ $colors=[
     x-transition:leave="transition ease-in duration-200"
     x-transition:leave-start="translate-x-0 opacity-100"
     x-transition:leave-end="translate-x-full opacity-0"
-    x-init="setTimeout(()=>show=false,3000)"
+    x-init="setTimeout(() => show = false, 3000)"
     class="fixed top-5 right-5 z-[9999]"
 >
 
-    <div class="toast {{ $colors[$type] }}">
+    <div class="toast {{ $classes[$type] }}">
+
+        <div class="toast-icon">
+            {{ $icons[$type] }}
+        </div>
 
         <div class="flex-1">
 
-            {{ $message }}
+            <div class="toast-title">
+                {{ $title }}
+            </div>
+
+            <div class="toast-message">
+                {{ $message }}
+            </div>
+
+            <div class="toast-progress"></div>
 
         </div>
 
         <button
-            type="button"
-            @click="show=false"
             class="toast-close"
+            @click="show=false"
+            type="button"
         >
             ✕
         </button>
