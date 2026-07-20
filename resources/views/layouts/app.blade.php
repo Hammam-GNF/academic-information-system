2ff36bf1389ee17b8e23e0269d145399d3946acb
Hammam Ghina Nur Fauzi <hammamgonjil@gmail.com>
2026-07-21 06:13:27 +0700

    feat(notification): register global toast component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,6 @@
 
         @include('layouts.navigation')
 
-        <x-feedback.toast />
-
         @isset($header)
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
